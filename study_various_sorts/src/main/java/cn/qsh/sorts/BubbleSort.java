bcf27a61a2dc2690b521853c255993a4fd2b227c
qushihao <qushihao@inibiru.com>
2022-12-08 17:28:01 +0800

    提交排序

--- study_various_sorts/src/main/java/cn/qsh/sorts/BubbleSort.java
+++ study_various_sorts/src/main/java/cn/qsh/sorts/BubbleSort.java
@@ -20,12 +20,11 @@ public class BubbleSort implements IArraySort {
         for (int i = 1; i < arr.length; i++) {
             //设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序
             boolean flag = true;
-            for (int j = 0; j < arr.length-1; j++) {
+            for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
-
                     flag = false;
                 }
             }
